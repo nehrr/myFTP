cee0c2bdf31f5da1df4c5bfb1da2bc18f31d04e9
nehrr <iamkapitaan@gmail.com>
2018-06-14 17:56:51 +0200

    put get function + log

--- server/main.js
+++ server/main.js
@@ -1,19 +1,84 @@
-const net = require("net");
+// const net = require("net");
+const fs = require("fs");
+const db = require("./db.json");
 
-const server = net.createServer(c => {
-  // 'connection' listener
-  console.log("client connected");
-  c.on("end", () => {
-    console.log("client disconnected");
+function checkUser(username) {
+  //checks for login
+  for (let item of db) {
+    if (item.user == username) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkPw(user, password) {
+  if (checkUser(user) == true) {
+    for (item of db) {
+      if (item.password == password) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+function list(user, directory) {
+  //returns items in directory
+  if (fs.existsSync(`./share/${user}/${directory}/`)) {
+    fs.readdir(`./share/${user}/${directory}/`, (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    });
+  } else {
+    fs.readdir(`./share/${user}/`, (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    });
+  }
+}
+
+function get(filename) {
+  //returns file
+  fs.copyFile(`./share/${user}/${filename}`, `${filename}`, err => {
+    if (err) throw err;
+    console.log("file was copied");
   });
-  c.write("Beep boop I'm a roboto server!\r\n");
-  c.pipe(c);
-});
+}
 
-server.on("error", err => {
-  throw err;
-});
+function put(filename) {
+  //returns nothing
+  fs.copyFile(`${filename}`, `./share/hawke/${filename}`, err => {
+    if (err) throw err;
+    console.log("file was added");
+  });
+}
 
-server.listen(4242, () => {
-  console.log("server bound");
-});
+function cwd(directory) {
+  //changes directory
+}
+
+console.log(checkUser("hawke"));
+console.log(checkPw("hawke", "nope"));
+list("hawke", "share");
+// get("hello.txt");
+// put("boop.txt");
+
+//
+// const server = net.createServer(c => {
+//   // 'connection' listener
+//   console.log("client connected");
+//   c.on("end", () => {
+//     console.log("client disconnected");
+//   });
+//   c.write("Beep boop I'm a roboto server!\r\n");
+//   c.pipe(c);
+// });
+//
+// server.on("error", err => {
+//   throw err;
+// });
+//
+// server.listen(4242, () => {
+//   console.log("server bound");
+// });
