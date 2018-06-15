7fa097b8dc93ce090bf73c0c63a1fb1f484b6e48
nehrr <iamkapitaan@gmail.com>
2018-06-15 16:37:20 +0200

    removed old

--- utils.js
+++ utils.js
@@ -33,3 +33,58 @@ export function isAllowedCommand(cmd) {
   //return index, if -1 means it does not exist
   return cmds.indexOf(cmd) !== -1;
 }
+
+/* old
+
+function checkUser(username) {
+  //checks for login
+  for (let item of db) {
+    if (item.user == username) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkPw(user, password) {
+  if (checkUser(user) == true) {
+    for (item of db) {
+      if (item.password == password) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+function list(user, directory) {
+  //returns items in directory
+  if (fs.existsSync(`./share/${user}/${directory}/`)) {
+    fs.readdir(`./share/${user}/${directory}/`, (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    });
+  } else {
+    fs.readdir(`./share/${user}/`, (err, data) => {
+      if (err) throw err;
+      console.log(data);
+    });
+  }
+}
+
+function get(filename) {
+  //returns file
+  fs.copyFile(`./share/${user}/${filename}`, `~/Desktop/${filename}`, err => {
+    if (err) throw err;
+    console.log("file was copied");
+  });
+}
+
+function put(filename) {
+  //returns nothing
+  fs.copyFile(`${filename}`, `./share/hawke/${filename}`, err => {
+    if (err) throw err;
+    console.log("file was added");
+  });
+}
+*/
